73a6844f97b79a12b3c329d0e08a153e00ee01c1
Cheng Tan <c.tan@northeastern.edu>
2024-12-20 09:43:33 -0500

    25spring

---
+++ env.sh
@@ -0,0 +1,16 @@
+export EGOS=$(pwd)
+
+# lab1-ex1:
+
+# if you use MacOS, you should uncomment the following two lines:
+# export PATH=$PATH:$EGOS/riscv64-unknown-elf-gcc-8.3.0-2020.04.1-x86_64-apple-darwin/bin
+# export PATH=$PATH:$EGOS/xpack-qemu-riscv-7.2.5-1/bin
+
+# # if you use Ubuntu on X86 CPUs (like Intel and AMD), you should uncomment the following two lines:
+# export PATH=$PATH:$EGOS/riscv64-unknown-elf-gcc-8.3.0-2020.04.1-x86_64-linux-ubuntu14/bin
+# export PATH=$PATH:$EGOS/xpack-qemu-riscv-7.2.5-1-linux-x64/bin
+
+# # if you're using other OSes, please fill in XXX below
+# # (also use this for troubleshooting)
+# export PATH=$PATH:$EGOS/riscv64-unknown-elf-gcc-XXX/bin
+# export PATH=$PATH:$EGOS/riscv-qemu-XXX/bin
